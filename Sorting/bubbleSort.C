9d3dd0b656eb89b8a37d4a8752c6e5360f976fec
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2021-05-01 17:12:05 +0530

    logic improved

--- Sorting/bubbleSort.C
+++ Sorting/bubbleSort.C
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int Arr[9] = {17, 14, 13, 11, 6, 3, 2, 1, 9};; 
+    int Arr[9] = {169, 14, 13, 11, 6, 3, 2, 1, 9};
     //int Arr[5] = {64, 25, 12, 22, 11};
-    int loopMaintainer, iterator, NextEle, lenght, temp, sortUpto;
+    int loopMaintainer, iterator, lenght, temp, sortUpto;
 
     lenght = sizeof(Arr)/sizeof(Arr[0]);
     
-    printf("Sorting using Bubble Sort Algorithm ");
+    printf("Sorting using bubble Sort Algorithm");
     printf("\n Lenght of the Array : %d", lenght);
     printf("\n Before Sorting : ");
     for ( loopMaintainer=0; loopMaintainer < lenght; loopMaintainer++) printf("%d \t", Arr[loopMaintainer]);
@@ -19,19 +19,18 @@ int main()
     {
         for (iterator=0; iterator < sortUpto; iterator++)
         {
-            NextEle = iterator + 1;
-            if (Arr[iterator] > Arr[NextEle])
+            if (Arr[iterator] > Arr[iterator + 1]) 
             {
-                temp = Arr[NextEle];
-                Arr[NextEle] = Arr[iterator];
-                Arr[iterator] = temp;  
+                temp = Arr[iterator + 1];
+                Arr[iterator + 1] = Arr[iterator];
+                Arr[iterator] = temp;
             }
         }
         sortUpto = sortUpto - 1;
     }
 
     printf("\n After Sorting : ");
-
+    //lenght = sizeof(Arr)/sizeof(Arr[0]);
     for (loopMaintainer=0; loopMaintainer < lenght; loopMaintainer++) printf("%d \t", Arr[loopMaintainer]);
     
     return 0;
